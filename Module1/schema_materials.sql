eedf4fa9aa02fe8f8d0f5fb8990deb0e81d9a6d2
deepthikantam <deepthikantam3388@gmail.com>
2025-12-11 18:15:33 +0530

    Delete Module1/schema_materials.sql

--- Module1/schema_materials.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Materials (
-    Material_ID SERIAL PRIMARY KEY,
-    Material_Type VARCHAR(50),
-    Strength NUMERIC,
-    Weight_Capacity NUMERIC,
-    Biodegradability_Score NUMERIC,
-    CO2_Emission_Score NUMERIC,
-    Recyclability_Percent NUMERIC
-);
