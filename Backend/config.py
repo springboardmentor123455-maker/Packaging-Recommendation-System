94e294b5753c606f4b35bd545d1bcc2c6ebc53fd
Ayush Kumar Pandey <ayushpandey1974@gmail.com>
2026-05-27 13:36:59 +0530

    Added SQLite fallback for Render

--- Backend/config.py
+++ Backend/config.py
@@ -4,10 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    uri = os.getenv("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "SQLALCHEMY_DATABASE_URI",
+        "sqlite:///database.db"
+    )
 
-    if uri and uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://", 1)
-
-    SQLALCHEMY_DATABASE_URI = uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
